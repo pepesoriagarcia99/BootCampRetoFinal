acfff7bf29721d7104e4b22c4ed5d1ea3039db0b
barmardom <bmardom@gmail.com>
2019-07-05 10:12:34 +0200

    Respositorio - Añadida query de listado al respositorio

--- back/src/main/java/org/springframework/samples/petclinic/repository/OfertaRespository.java
+++ back/src/main/java/org/springframework/samples/petclinic/repository/OfertaRespository.java
@@ -1,12 +1,18 @@
 package org.springframework.samples.petclinic.repository;
 
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.samples.petclinic.model.Oferta;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OfertaRespository extends JpaRepository<Oferta, Integer>{
 	
+	@Query("SELECT o FROM Ofertas o WHERE o.fechaExpiracion > ?1")
+	List<Oferta> findByfechaExpiracion(Date fechaActual);
 
 }
