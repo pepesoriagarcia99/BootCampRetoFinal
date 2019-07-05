37898b533f1b35ead5951ef4cf2d1fd5988d4054
pepesoriagarcia99 <pepesoriagarcia99@gmail.com>
2019-07-05 11:53:21 +0200

    nueva rama front, merge solucion

--- front/src/app/ofertas/service/oferta.service.ts
+++ front/src/app/ofertas/service/oferta.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { OfertaInterface } from './oferta.interface';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { OfertaDto } from './oferta.dto';
+
+const Url = environment.REST_API_URL;
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfertaService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll(): Observable<OfertaInterface>{
+    return this.http.get<OfertaInterface>(`${Url}/ofertas`);
+  }
+  getOne(){
+
+  }
+  create(oferta: OfertaDto): Observable<OfertaInterface>{
+    return this.http.post<OfertaInterface>(`${Url}/ofertas`, oferta);
+  }
+  delete(id: Number){
+    return this.http.delete<OfertaInterface>(`${Url}/ofertas/${id}`);
+  }
+  edit(oferta: OfertaDto, id: Number){
+    return this.http.put<OfertaInterface>(`${Url}/ofertas/${id}`, oferta);
+  }
 }
